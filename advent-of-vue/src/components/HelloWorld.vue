1c0ec144f838555c5d40080c900ffacfaeba6297
Daniel Gee <dan@dan-gee.co.uk>
2022-12-05 21:29:59 +0000

    Add app structure

--- advent-of-vue/src/components/HelloWorld.vue
+++ advent-of-vue/src/components/HelloWorld.vue
@@ -1,79 +1,34 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <v-img
-        contain
-        height="300"
-        src="src/assets/logo.svg"
-      />
+  <v-app :theme="theme">
+    <v-app-bar>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-spacer />
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+      <v-btn
+        :prepend-icon="
+          theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'
+        "
+        @click="onClick"
+        >Toggle Theme</v-btn
+      >
+    </v-app-bar>
 
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+    <v-navigation-drawer v-model="drawer" temporary />
 
-      <div class="py-14" />
-
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            href="https://next.vuetifyjs.com/components/all/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
-
-            Components
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            href="https://next.vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
-          >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
-
-            Get Started
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            href="https://community.vuetifyjs.com/"
-            min-width="164"
-            rel="noopener noreferrer"
-            target="_blank"
-            variant="text"
-          >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
-
-            Community
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-responsive>
-  </v-container>
+    <v-main>
+      <v-container>Content area</v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup lang="ts">
-  //
+import { ref } from "vue";
+
+const theme = ref("light");
+
+function onClick() {
+  theme.value = theme.value === "light" ? "dark" : "light";
+}
+
+const drawer = ref(false);
 </script>
