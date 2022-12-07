74a7c94b496c988e48592d83f2bd90171760df1d
Daniel Gee <dan@dan-gee.co.uk>
2022-12-07 18:40:33 +0000

    finish off christmas tree

--- advent-of-vue/src/components/aov-components/2022/ChristmasTree.vue
+++ advent-of-vue/src/components/aov-components/2022/ChristmasTree.vue
@@ -2,9 +2,9 @@
   <div>
     <ChristmasTree v-if="size > 1" :size="size - 1" />
 
-    <div class="flex flex-row justify-center">
+    <div class="d-flex justify-center">
       <!-- Create the tree sections -->
-      <v-icon icon="mdi-checkbox-blank-circle" v-for="i in size" :key="`${size}-${i}`"  class="" />
+      <v-icon icon="mdi-checkbox-blank-circle" v-for="i in size" :key="`${size}-${i}`" color="green" size="60px" style="margin: -10px;" />
     </div>
   </div>
 </template>
