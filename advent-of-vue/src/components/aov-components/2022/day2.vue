ad11033a392ef69b1b379b6d5724d16c3469ddba
Daniel Gee <dan@dan-gee.co.uk>
2022-12-07 07:23:28 +0000

    Add types

--- advent-of-vue/src/components/aov-components/2022/day2.vue
+++ advent-of-vue/src/components/aov-components/2022/day2.vue
@@ -14,13 +14,33 @@
 <script setup lang="ts">
 import { ref } from "vue";
 type JokeState = "requesting" | "new" | "answer";
+
+type JokeResponse = {
+  error: boolean;
+  category: string;
+  type: string;
+  setup: string;
+  delivery: string;
+  flags: {
+    nsfw: boolean;
+    religious: boolean;
+    political: boolean;
+    racist: boolean;
+    sexist: boolean;
+    explicit: boolean;
+  };
+  id: number;
+  safe: boolean;
+  lang: string;
+};
+
 const jokeState = ref("gettingJoke" as JokeState);
 
 async function getNewJoke() {
   jokeState.value = "requesting";
   const result = await (
     await fetch("https://v2.jokeapi.dev/joke/christmas")
-  ).json();
+  ).json() as JokeResponse;
   jokeSetup.value = result.setup;
   jokeResponse.value = result.delivery;
   jokeState.value = "new";
