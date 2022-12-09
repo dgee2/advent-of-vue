dbc712ea9e5e548af45641ae4c8885391bf416ef
Daniel Gee <dan@dan-gee.co.uk>
2022-12-09 17:14:21 +0000

    Add day 5

---
+++ advent-of-vue/src/components/aov-components/2022/day5.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-form style="width: 300px;">
+    <v-text-field v-model="displayTo" readonly solo />
+    <v-text-field v-model="displayFrom" readonly solo />
+  </v-form>
+  <v-form>
+    <v-text-field v-model="to" label="First name" />
+    <v-text-field v-model="from" label="Last name" />
+  </v-form>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+const to = ref("");
+const from = ref("");
+
+const displayTo = computed(() => `To: ${to.value}`);
+const displayFrom = computed(() => `From: ${from.value}`);
+</script>
+
+<style scoped></style>
--- advent-of-vue/src/services/exercises.ts
+++ advent-of-vue/src/services/exercises.ts
@@ -2,11 +2,13 @@ import day1Vue from "@/components/aov-components/2022/day1.vue"
 import day2Vue from "@/components/aov-components/2022/day2.vue"
 import day3Vue from "@/components/aov-components/2022/day3.vue"
 import day4Vue from "@/components/aov-components/2022/day4.vue"
+import day5Vue from "@/components/aov-components/2022/day5.vue"
 
 export default [
     { name: 'Day 1', path: '/2022/day-1', component: day1Vue },
     { name: 'Day 2', path: '/2022/day-2', component: day2Vue },
     { name: 'Day 3', path: '/2022/day-3', component: day3Vue },
     { name: 'Day 4', path: '/2022/day-4', component: day4Vue },
+    { name: 'Day 5', path: '/2022/day-5', component: day5Vue },
 ]
 
